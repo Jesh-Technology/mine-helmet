6c3ec443eda86c7cdaa419e1c6c2bb90fafc2cc9
Jesh-Technology <114339951+Jesh-Technology@users.noreply.github.com>
2023-04-12 15:01:24 +0530

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,8 +3,8 @@ import react from 'react'
 function Map(){
     return(
         <div>
-            <div class="ratio ratio-16x9">
-  <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3949.14202042773!2d77.415974!3d8.188629!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwMTEnMTguNCJOIDc3wrAyNCc1Ni45IkU!5e0!3m2!1sen!2sin!4v1681284615638!5m2!1sen!2sin" title="YouTube video" allowfullscreen></iframe>
+            <div class="ratio ratio-16x9 rounded-4">
+  <iframe className="rounded-4" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3949.14202042773!2d77.415974!3d8.188629!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwMTEnMTguNCJOIDc3wrAyNCc1Ni45IkU!5e0!3m2!1sen!2sin!4v1681284615638!5m2!1sen!2sin" title="YouTube video" allowfullscreen></iframe>
 </div>
         </div>
     )
