4649222f24ca92e2ae13b649f9e782e17fe4ccad
Jesh-Technology <114339951+Jesh-Technology@users.noreply.github.com>
2023-04-13 00:21:32 +0530

    Update adminlogin.js

--- src/pages/adminlogin.js
+++ src/pages/adminlogin.js
@@ -2,6 +2,9 @@
 import database from "@/server/firebase";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Head from 'next/head';
+import Link from "next/link"
+import Script from "next/script"
 
 function Adminlogin() {
     const [AUTH, setAUTH] = useState()
@@ -38,27 +41,84 @@ function Adminlogin() {
     }
   
     return (
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value.toString())}
-          />
+
+      <div class="bg-dark" data-bs-theme="dark" style={{"backgroundColor":"#212529"}}>
+      <Head>
+        <title>Home</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous"/>
+  
+      </Head>
+      <div style={{"backgroundColor":"#212529"}}>
+      <main class="form-signin">
+      <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand mx-auto fw-bolder" href="#">
+      Magnatite
+    </a>
+  </div>
+</nav>
+      <form onSubmit={handleSubmit} class="m-4">
+        <div class="form-box mx-auto " style={{"width":"25%","height":"100%"}}>
+          <div class="form-floating mb-3">
+            <input class="form-control shadow-none" autocomplete="off" required type="email" id="email" value={email} onChange={(event) => setEmail(event.target.value.toString())} placeholder="E-mail"/>
+            <label class="text-secondary fw-bold" for="floatingInput">Email address</label>
+          </div>
+
+          <div class="form-floating">
+            <input class="form-control shadow-none" autocomplete="off" required type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value.toString())} placeholder="Password"/>
+            <label class="text-secondary fw-bold" for="floatingPassword">Password</label>
+          </div>
+          <div class="d-grid gap-2 my-3">
+  <button class="btn btn-primary btn-lg fw-bold" type="submit">Login</button>
+</div>
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value.toString())}
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+        </form>
+      </main>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossOrigin="anonymous"></script>
+
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // <form onSubmit={handleSubmit}>
+      //   <div>
+      //     <label htmlFor="email">Email:</label>
+      //     <input
+      //       type="email"
+      //       id="email"
+      //       value={email}
+      //       onChange={(event) => setEmail(event.target.value.toString())}
+      //     />
+      //   </div>
+      //   <div>
+      //     <label htmlFor="password">Password:</label>
+      //     <input
+      //       type="password"
+      //       id="password"
+      //       value={password}
+      //       onChange={(event) => setPassword(event.target.value.toString())}
+      //     />
+      //   </div>
+      //   <button type="submit">Submit</button>
+      // </form>
     )
   }
 export default Adminlogin
